615e9c17441a4197235c6b47146592b2fe822b54
martijnkunstman <martijn@kunstman.nl>
2021-01-18 14:09:10 +0100

    express

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+console.log("start express");
 
 const express = require('express');
 const app = express(); 
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 app.use(express.static('static'));
-app.listen(process.env.PORT || 3000); 
+
+//const socket = require('socket.io')(5000)
+//const io = socket(server)
 
 const io = require('socket.io')(5000)
+
 
 io.on('connection', socket => {
   const id = socket.handshake.query.id
   socket.join(id)
+  console.log("socket connection");
+
+
 
   socket.on('send-message', ({ recipients, text }) => {
     recipients.forEach(recipient => {
